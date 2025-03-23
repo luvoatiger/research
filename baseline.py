5facb6b5ea0966082db02f50692ef329ec38ca50
kkb0115 <kkb0115@ex-em.com>
2025-03-23 11:29:40 +0900

    upload baseline, simulation, test

---
+++ baseline.py
@@ -0,0 +1,25 @@
+import os
+
+import numpy as np
+import pandas as pd
+import torch
+import torchdiffeq
+
+import matplotlib.pyplot as plt
+
+
+def conduct_experiment():
+    pass
+
+if __name__ == "__main__":
+    length_to_integrate_M = 5
+    batch_size = 100
+    num_epoch = 2000
+
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+
+    for epoch in range(num_epoch):
+        pass
+
+
+
